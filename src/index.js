f304f7ea79444ddab0ad39670f347b2bcb4d1046
Zoila <zoila.aliaga.87@gmail.com>
2022-06-08 16:33:35 -0500

    validación y maskify funcionado de manera correcta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+//import { resetDefaultConfig } from 'eslint/lib/rule-tester/rule-tester';
 import validator from './validator.js';
 
+const comprar= document.querySelector('#comprar');
+const validar=document.querySelector('#validar')
+let creditCard=document.getElementById('creditCard');
+const result=document.getElementById('result')
+
+
+comprar.addEventListener("click", (e)=> {
+  e.preventDefault();
+    document.getElementById("formulario").style.display = "block";
+    document.getElementById("peliculas").style.display = "none";
+});
+
+validar.addEventListener("click", (e)=> {
+  e.preventDefault();
+  if(validator.isValid(creditCard.value)===true){
+    result.innerHTML="Tu tarjeta es válida, continúa tu compra"
+  }else{
+    result.innerHTML="Lo sentimos tu tarjeta no es válida, por favor intenta con otra tarjeta"
+  }
+    
+    console.log(validator.isValid(creditCard.value))
+    console.log(validator.maskify(creditCard.value))
+ 
+      
+});
+
+    
 console.log(validator);
+
+
+
+
+
+
+  
+ 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
-};
+  isValid:(creditCard)=>{
+    console.log(creditCard);
+
+    let inverse=creditCard.split("").reverse();
+    console.log(inverse); 
+    let result =0; 
+    
+
+    for(let i=0; i<inverse.length; i++){
+      if(i%2===1)
+      { 
+        inverse[i] *=2;
+        
+        if(inverse[i]>9){
+          inverse[i] -=9;
+          console.log(inverse[i]);
+        }        
+
+      }
+      result +=parseInt(inverse[i]);
+      console.log(result)      
+    }
+    return(result%10)===0;
+       
+  },
+
+  maskify:(creditCard)=>{
+    if(creditCard.length<4){
+      return creditCard
+    }else{
+      let ultimos_4=''
+      for (let i=creditCard.length-4;i<creditCard.length;i++)
+      ultimos_4 +=creditCard[i]
+
+      let mask = ''
+    for (let j =0; j<creditCard.length; j++)
+    mask += '#'
+
+    return mask + ultimos_4
+    }
+    
+    
+
+  }
+  
+}
+
+
 
 export default validator;
